4085f7e3b34b5864c80adaf76e8ef1effb467600
kevin <kevina.rojasr@konradlorenz.edu.co>
2024-10-21 03:25:03 -0500

    se agrega el taller completo con sus respectivas clases

---
+++ Circulo.java
@@ -0,0 +1,28 @@
+public class Circulo extends FiguraGeometrica {
+    private double radio;
+
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double obtenerArea() {
+        // Complejidad temporal: O(1) Tiempo constante.
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        // Complejidad temporal: O(1) Tiempo constante.
+        return 2 * Math.PI * radio;
+    }
+}
---
+++ Triangulo.java
@@ -0,0 +1,39 @@
+public class Triangulo extends FiguraGeometrica {
+    private double base;
+    private double altura;
+
+    public Triangulo(String nombre, String color, double base, double altura) {
+        super(nombre, color);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public double obtenerArea() {
+        // Complejidad temporal: O(1) Tiempo constante.
+        return (base * altura) / 2;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        // Complejidad temporal: O(1) Tiempo constante.
+       
+        return 3 * base;
+    }
+}
